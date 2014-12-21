7a5da9fd2b4fb33a75525fe054f5dbe75d61eaeb
Raymond Jacobson <me@raymondjacobson.com>
2014-12-21 13:47:26 -0500

    add save text area action

--- pub/script/components.js
+++ pub/script/components.js
@@ -5,8 +5,9 @@
 
 var TextArea = React.createClass({
   getTextAreaContent: function () {
+    var fetch_url = this.props.url + "fetch/";
     $.ajax({
-      url: this.props.url,
+      url: fetch_url,
       dataType: 'json',
       success: function(data) {
         this.setState({data:data});
@@ -16,22 +17,39 @@ var TextArea = React.createClass({
       }.bind(this),
     });
   },
+  saveTextAreaContent: function () {
+    var edit_url = this.props.url + "edit/";
+    $.ajax({
+      url: edit_url,
+      dataType: 'json',
+      type: 'POST',
+      data: 'f'/* GET TEXT AREA TEXT */,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
   getInitialState: function() {
     return {data: []};
   },
   componentDidMount: function() {
     this.getTextAreaContent();
-    setInterval(this.getTextAreaContent, this.props.pollInterval);
+    setInterval(this.saveTextAreaContent, this.props.savePollInterval);
   },
   render: function() {
-    console.log(this.state.data);
-    return (
-      <h1>{this.state.data[1]}</h1>
-    );
+    console.log(typeof this.state.data[1]);
+    if (typeof (this.state.data[1]) != undefined) {
+      return (
+        <h1>{this.state.data[1]}</h1>
+      )
+    }
   }
 });
 
 React.render(
-  <TextArea url="/page/f/fetch" pollInterval={2000} />,
+  <TextArea url="/page/f/" savePollInterval={2000} />,
   document.getElementById('content')
 );
