2140ba20d109f424c6dade188642460b1ac03fb2
Raymond Jacobson <me@raymondjacobson.com>
2014-12-21 02:38:13 -0500

    move in some reaction

--- pub/script/components.js
+++ pub/script/components.js
@@ -4,14 +4,34 @@
  */
 
 var TextArea = React.createClass({
+  getTextAreaContent: function () {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      success: function(data) {
+        this.setState({data:data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this),
+    });
+  },
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    this.getTextAreaContent();
+    setInterval(this.getTextAreaContent, this.props.pollInterval);
+  },
   render: function() {
+    console.log(this.state.data);
     return (
-      <h1>hello</h1>
-    );    
+      <h1>{this.state.data[1]}</h1>
+    );
   }
 });
 
 React.render(
-  <TextArea url="sample.json" pollInterval={2000} />,
+  <TextArea url="/page/f/fetch" pollInterval={2000} />,
   document.getElementById('content')
 );
