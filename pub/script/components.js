44416d56835e9966af87d706e328a83c236d3e76
Raymond Jacobson <me@raymondjacobson.com>
2014-12-21 02:28:20 -0500

    add post route, structure react

--- pub/script/components.js
+++ pub/script/components.js
@@ -2,3 +2,16 @@
  * White Blank Page
  * Raymond Jacobson 2014
  */
+
+var TextArea = React.createClass({
+  render: function() {
+    return (
+      <h1>hello</h1>
+    );    
+  }
+});
+
+React.render(
+  <TextArea url="sample.json" pollInterval={2000} />,
+  document.getElementById('content')
+);
--- server.go
+++ server.go
@@ -11,23 +11,36 @@ import (
   "log"
 )
 
+// Route for the users white blank page
+// Check for the cookie set on the browser, else set cookie with UID
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
   http.ServeFile(w, r, "index.html");
 }
 
-func Page(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+// Temporary dummy route to serve json data
+func PageFetch(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+  w.Header().Set("Content-Type", "application/json")
+  http.ServeFile(w, r, "sample.json");
+}
+
+// Temporary route to handle post request for new data coming in
+func PageEdit(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
   w.Header().Set("Content-Type", "application/json")
   http.ServeFile(w, r, "sample.json");
 }
 
 func main() {
+
   router := httprouter.New()
+
+  // Serve static assets
   router.ServeFiles("/pub/*filepath", http.Dir("pub/"))
+
+  // Routes
   router.GET("/", Index)
-  router.GET("/page/:key", Page)
+  router.GET("/page/:key/fetch", PageFetch)
+  router.POST("/page/:key/edit", PageEdit)
 
   log.Fatal(http.ListenAndServe(":3000", router))
+  
 }
-
-
-
